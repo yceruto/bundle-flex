e48f73a0a27d78ba533b087c3b768c5d4b6fe69f
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-12-08 15:19:12 -0500

    configure bundle files and update composer.json

--- src/Flex.php
+++ src/Flex.php
@@ -5,7 +5,10 @@ namespace Yceruto\BundleFlex;
 use Composer\Composer;
 use Composer\EventDispatcher\Event;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Factory;
 use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
+use Composer\Json\JsonManipulator;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\ScriptEvents;
 
@@ -35,7 +38,22 @@ class Flex implements PluginInterface, EventSubscriberInterface
 
     public function configureBundle(Event $event): void
     {
-        // Remove LICENSE (which do not apply to the user bundle)
+        // Remove files that do not apply to the user bundle
         @unlink('LICENSE');
+        @unlink('README.md');
+
+        $this->configureComposerJson();
+    }
+
+    private function configureComposerJson(): void
+    {
+        $file = Factory::getComposerFile();
+        $contents = file_get_contents($file);
+        JsonFile::parseJson($contents);
+
+        $manipulator = new JsonManipulator($contents);
+        $manipulator->addProperty('name', 'acme/acme-bundle');
+        $manipulator->addProperty('description', 'Acme bundle description');
+        file_put_contents($file, $manipulator->getContents());
     }
 }
