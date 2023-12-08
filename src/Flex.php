e7361de38a59c4b5a8ad65c94f2064b0a214b6e6
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-12-08 15:52:36 -0500

    reinstall after remove the plugin

--- src/Flex.php
+++ src/Flex.php
@@ -2,11 +2,15 @@
 
 namespace Yceruto\BundleFlex;
 
+use Composer\Command\GlobalCommand;
 use Composer\Composer;
 use Composer\EventDispatcher\Event;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Factory;
+use Composer\Installer;
+use Composer\Installer\SuggestedPackagesReporter;
 use Composer\IO\IOInterface;
+use Composer\IO\NullIO;
 use Composer\Json\JsonFile;
 use Composer\Json\JsonManipulator;
 use Composer\Package\Locker;
@@ -18,6 +22,7 @@ class Flex implements PluginInterface, EventSubscriberInterface
     private static bool $activated = true;
     private Composer $composer;
     private IOInterface $io;
+    private Installer $installer;
 
     public static function getSubscribedEvents(): array
     {
@@ -34,6 +39,13 @@ class Flex implements PluginInterface, EventSubscriberInterface
     {
         $this->composer = $composer;
         $this->io = $io;
+
+        $backtrace = debug_backtrace();
+        foreach ($backtrace as $trace) {
+            if (isset($trace['object']) && $trace['object'] instanceof Installer) {
+                $this->installer = $trace['object']->setSuggestedPackagesReporter(new SuggestedPackagesReporter(new NullIO()));
+            }
+        }
     }
 
     public function deactivate(Composer $composer, IOInterface $io): void
@@ -48,6 +60,7 @@ class Flex implements PluginInterface, EventSubscriberInterface
 
     public function configureBundle(Event $event): void
     {
+        var_dump($event->getArguments());
         $this->removeSkeletonFiles();
         $this->configureComposerJson();
     }
@@ -71,7 +84,8 @@ class Flex implements PluginInterface, EventSubscriberInterface
         $manipulator->removeSubNode('config', 'allow-plugins');
         file_put_contents($file, $manipulator->getContents());
 
-        $this->updateComposerLock();
+        //$this->updateComposerLock();
+        $this->reinstall();
     }
 
     private function updateComposerLock(): void
@@ -83,5 +97,28 @@ class Flex implements PluginInterface, EventSubscriberInterface
         $lockData = $locker->getLockData();
         $lockData['content-hash'] = Locker::getContentHash($composerJson);
         $lockFile->write($lockData);
+    }
+
+    private function reinstall(): void
+    {
+        $composer = Factory::create($this->io, null, false, true);
+
+        $installer = clone $this->installer;
+        $installer->__construct(
+            $this->io,
+            $composer->getConfig(),
+            $composer->getPackage(),
+            $composer->getDownloadManager(),
+            $composer->getRepositoryManager(),
+            $composer->getLocker(),
+            $composer->getInstallationManager(),
+            $composer->getEventDispatcher(),
+            $composer->getAutoloadGenerator()
+        );
+        if (method_exists($installer, 'setPlatformRequirementFilter')) {
+            $installer->setPlatformRequirementFilter(((array) $this->installer)["\0*\0platformRequirementFilter"]);
+        }
+
+        $installer->run();
     }
 }
