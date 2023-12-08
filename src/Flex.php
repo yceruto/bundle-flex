cf4e199f0b06d4e06af206c98cb214c5fde694cf
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-12-08 15:32:04 -0500

    remove bundle-flex and allow-plugins nodes

--- src/Flex.php
+++ src/Flex.php
@@ -67,6 +67,8 @@ class Flex implements PluginInterface, EventSubscriberInterface
         $manipulator = new JsonManipulator($contents);
         $manipulator->addProperty('name', 'acme/acme-bundle');
         $manipulator->addProperty('description', 'Acme bundle description');
+        $manipulator->removeSubNode('require', 'yceruto/bundle-flex');
+        $manipulator->removeSubNode('config', 'allow-plugins');
         file_put_contents($file, $manipulator->getContents());
 
         $this->updateComposerLock();
